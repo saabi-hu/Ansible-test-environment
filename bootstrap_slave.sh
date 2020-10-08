c20c87f8e8b9821aff462553ca5c9f0716bd51aa
Szabolcs Regős <saabi-hu@users.noreply.github.com>
2020-10-09 00:20:40 +0200

    SSH connection established
    ansible user created
    ansible@master.local user's SSH key created
    ansible@master.local's SSH key transferred to ansible@node*.local using TFTP
    ansible@master.local can log into the ansible@node*.local

--- bootstrap_slave.sh
+++ bootstrap_slave.sh
@@ -2,6 +2,10 @@
 
 dnf config-manager --set-enabled PowerTools
 dnf --assumeyes install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-dnf --assumeyes install avahi nss-mdns
+dnf --assumeyes install avahi nss-mdns tftp
 
 systemctl enable avahi-daemon --now
+useradd --comment "Ansible account" --create-home --user-group ansible
+sudo -u ansible tftp master.local -c get id_ed25519.pub ~ansible/master-id_25519.pub
+sudo -u ansible mkdir ~ansible/.ssh/
+sudo -u ansible sh -c "cat ~ansible/master-id_25519.pub > ~ansible/.ssh/authorized_keys"
